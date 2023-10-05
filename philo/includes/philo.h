c0e9ecff0e3fda4697725dfac5892367de749654
Hayato Sawamura <hsawamur@c1r15s4.42tokyo.jp>
2023-10-05 22:52:19 +0900

    大まかなメンバ変数

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,13 +6,54 @@
 /*   By: hsawamur <hsawamur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 13:50:59 by hsawamur          #+#    #+#             */
-/*   Updated: 2023/10/05 18:16:06 by hsawamur         ###   ########.fr       */
+/*   Updated: 2023/10/05 22:51:48 by hsawamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# define MESSAGE_TAKEN_A_FORK "timestamp_in_ms X has taken a fork"
+# define MESSAGE_EATING "timestamp_in_ms X is eating"
+# define MESSAGE_SLEEPING "timestamp_in_ms X is sleeping"
+# define MESSAGE_THINKING "timestamp_in_ms X is thinking"
+# define MESSAGE_DIED "timestamp_in_ms X died"
+
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+enum philo_status {
+	DEFAULT,
+	EATING,
+	SLPEENG,
+	THINKING,
+	DIED
+}
+
+typedef struct e_philo_ability {
+	ssize_t	count;
+	int		die_time;
+	int		eat_time;
+	int		sleep_time;
+}	t_philo_ability;
+
+typedef struct e_philo {
+	int				id;
+	struct e_fork	left;
+	struct e_fork	right;
+	t_philo_ability	ability;
+}	t_philo;
+
+
+typedef struct e_fork {
+	pthread_mutex_t	fork;
+	bool			is_used;
+}	t_fork;
+
+typedef struct e_table {
+	t_philo	*philos;
+	t_fork	*forks;
+}	t_table;
 
 #endif
