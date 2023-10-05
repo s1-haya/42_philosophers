5c6c45085c33544060975f872a50027d32eb9512
Hayato Sawamura <hsawamur@c1r15s1.42tokyo.jp>
2023-10-05 18:32:46 +0900

    create main

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,9 +6,13 @@
 /*   By: hsawamur <hsawamur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 13:50:59 by hsawamur          #+#    #+#             */
-/*   Updated: 2023/10/05 15:50:52 by hsawamur         ###   ########.fr       */
+/*   Updated: 2023/10/05 18:16:06 by hsawamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_H
+# define PHILO_H
 
+#include <stdio.h>
 
+#endif
