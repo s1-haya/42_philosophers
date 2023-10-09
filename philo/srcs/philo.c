9559c28abb11a08fc918874e994921074f092e53
Hayato Sawamura <hsawamur@c1r23s6.42tokyo.jp>
2023-10-09 14:27:02 +0900

    create exec philo file

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,14 +6,14 @@
 /*   By: hsawamur <hsawamur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 13:49:54 by hsawamur          #+#    #+#             */
-/*   Updated: 2023/10/05 18:20:56 by hsawamur         ###   ########.fr       */
+/*   Updated: 2023/10/09 14:22:30 by hsawamur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main(int argc, char **argv)
-{
-	printf("gogogo!");
-	return (0);
-}
+// int main(int argc, char **argv)
+// {
+// 	printf("gogogo!");
+// 	return (0);
+// }
